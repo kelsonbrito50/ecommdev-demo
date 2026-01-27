b94e50c2a230d5108a73533190863ba0f4748568
kelsonbrito50 <kelsonbrito50@users.noreply.github.com>
2026-01-27 17:12:03 -0600

    Simplify portfolio admin to fix 500 errors
    Removed complex display methods and problematic configurations
    that were causing server errors. Now matches servicos admin pattern.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -6,19 +6,29 @@ from .models import CategoriaPortfolio, Case, CaseImage, Tag
 @admin.register(CategoriaPortfolio)
 class CategoriaPortfolioAdmin(admin.ModelAdmin):
     list_display = ['nome_pt', 'slug', 'ordem']
+    list_editable = ['ordem']
     prepopulated_fields = {'slug': ('nome_pt',)}
     search_fields = ['nome_pt']
+    ordering = ['ordem']
+
+
+class CaseImageInline(admin.TabularInline):
+    model = CaseImage
+    extra = 3
+    fields = ['imagem', 'titulo', 'ordem']
     ordering = ['ordem']
 
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ['titulo_pt', 'categoria', 'cliente', 'destaque', 'ativo', 'ordem']
-    list_filter = ['destaque', 'ativo']
+    list_filter = ['categoria', 'destaque', 'ativo']
     list_editable = ['destaque', 'ativo', 'ordem']
     search_fields = ['titulo_pt', 'cliente']
     prepopulated_fields = {'slug': ('titulo_pt',)}
     ordering = ['-destaque', 'ordem']
+    inlines = [CaseImageInline]
+    save_on_top = True
 
     fieldsets = (
         (_('Identificação'), {
@@ -28,29 +38,36 @@ class CaseAdmin(admin.ModelAdmin):
             'fields': ('cliente', 'industria')
         }),
         (_('Conteúdo'), {
-            'fields': ('desafio_pt', 'solucao_pt', 'resultados_pt'),
-            'classes': ['wide']
+            'fields': ('desafio_pt', 'desafio_en', 'solucao_pt', 'solucao_en', 'resultados_pt', 'resultados_en')
         }),
         (_('Técnico'), {
-            'fields': ('tecnologias', 'funcionalidades', 'tempo_desenvolvimento'),
-            'classes': ['collapse']
+            'fields': ('tecnologias', 'funcionalidades', 'tempo_desenvolvimento')
         }),
         (_('Mídia'), {
-            'fields': ('imagem_destaque', 'url_projeto')
+            'fields': ('imagem_destaque', 'imagens', 'url_projeto')
+        }),
+        (_('Métricas'), {
+            'fields': ('metricas', 'visualizacoes')
         }),
         (_('Exibição'), {
             'fields': ('destaque', 'ativo', 'ordem')
         }),
     )
 
+    readonly_fields = ['visualizacoes']
+
 
 @admin.register(CaseImage)
 class CaseImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'titulo', 'ordem']
-    ordering = ['ordem']
+    list_display = ['id', 'case', 'titulo', 'ordem']
+    list_filter = ['case']
+    list_editable = ['ordem']
+    search_fields = ['titulo']
+    ordering = ['case', 'ordem']
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['nome', 'slug']
     prepopulated_fields = {'slug': ('nome',)}
+    search_fields = ['nome']
